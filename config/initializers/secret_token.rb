2bc16d9c87ecfa6426cfb632c45be8b541bbce61
Rui Vaz <rui.wax@gmail.com>
2014-01-17 22:42:54 -0800

    initialized MyHood repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myhood::Application.config.secret_key_base = '3f9693ecca9308fa8e8c6a7506b34615944874aa65129635f88b6d237771fd579a3df3c67229b46331608b424512873ec71914972536ebd0f2428a2fb3cd84fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Myhood::Application.config.session_store :cookie_store, key: '_myhood_session'
